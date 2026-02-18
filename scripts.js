e3fc42bf143c10df0bbd126229ef6f2e72cbfeb3
abizynelies <66037255+abizynelies@users.noreply.github.com>
2026-02-18 10:20:38 +0000

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -21,22 +21,7 @@ this.reset()
 })
 
 
-// show popup after 5 seconds
-setTimeout(function(){
-document.getElementById("mc_embed_shell").style.display="flex"
-},5000);
 
-// close popup when X is clicked
-document.querySelector(".close-btn").addEventListener("click",function(){
-document.getElementById("mc_embed_shell").style.display="none";
-})
-
-// close when clicking outside content
-document.getElementById("mc_embed_shell").addEventListener("click",function(e){
- if(e.target=== this){
-  this.style.display="none";
- }
-});
 
 
 // =======================> Cookies <=============================
@@ -53,11 +38,11 @@ function acceptCookies() {
 function declineCookies() {
   document.getElementById("cookie-banner").style.display ="none";
   localStorage.setItem("cookiesAccepted", "false")
-  loadAnalytics(); // GA4
+  // loadAnalytics(); // GA4
   
 }
 
-on page load, check past choice
+// on page load, check past choice
 
 window.onload = function () {
   
@@ -69,10 +54,3 @@ window.onload = function () {
     document.getElementById("cookie-banner").style.display="none";
   }
 }
-
-
-
-
-
-
-
